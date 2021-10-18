7a4b61969ad50b8e627e43f95a73a5e2d4e8c333
codedieme <ywcheah.2019@scis.smu.edu.sg>
2021-10-18 10:44:46 +0800

    Update db_connection.js

--- db_connection.js
+++ db_connection.js
@@ -2,32 +2,31 @@ var common = exports;
 var mysql = require("mysql");
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "spmproject",
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "spmproject",
 });
 
 
-con.connect(function(err) {
+con.connect(function (err) {
     if (err) throw err;
     con.query("SELECT * FROM Engineer", function (err, result, fields) {
-      console.log(result);
+        console.log(result);
     });
 });
 
-function getLastRecord(name)
-{
-    return new Promise(function(resolve, reject) {
+function getLastRecord(name) {
+    return new Promise(function (resolve, reject) {
         // The Promise constructor should catch any errors thrown on
         // this tick. Alternately, try/catch and reject(err) on catch.
         var connection = getMySQL_connection();
 
         var query_str =
-        "SELECT name, " +
-        "FROM engineer " +   
-        // "WHERE (name = ?) " +
-        "LIMIT 1 ";
+            "SELECT name, " +
+            "FROM engineer " +
+            // "WHERE (name = ?) " +
+            "LIMIT 1 ";
 
         var query_var = [name];
 
@@ -42,7 +41,8 @@ function getLastRecord(name)
     });
 }
 
-getLastRecord('name_record').then(function(rows) {
+getLastRecord('name_record').then(function (rows) {
     // now you have your rows, you can see if there are <20 of them
-}).catch((err) => setImmediate(() => { throw err; })); // Throw async to escape the promise chain
-  
+}).catch((err) => setImmediate(() => {
+    throw err;
+})); // Throw async to escape the promise chain
